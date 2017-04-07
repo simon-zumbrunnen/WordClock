31138b05f1c31bad66d077160082a112e628d149
seims <seims@192.168.0.241>
2017-04-07 18:52:45 +0200

    I2C communication of RTC works.

--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
 
 #include "config.h"
 #include "fuses.h"
+#include "time.h"
+#include "i2c.h"
+#include "rtc.h"
 #include "ledmatrix.h"
 
 #include <xc.h>
 
 uint8_t main( void )
-{      
+{
+    i2c_init();
+    rtc_init();
     ledmatrix_init();
-    datetime_t time = {2017, 04, 07, 14, 50, 12};
+    
+    time_t time = {14, 50, 12};
+    rtc_write_time(time);
+    time = rtc_read_time();
+    
     ledmatrix_set_time(time);
     
     while(1)
